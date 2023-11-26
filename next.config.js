b60d0b5d4e5d1f43629f481d6c07ebebc377195a
Siva Ganesh Manyam <siva.manyam@siva.manyam-JXFV0M7QKM>
2023-11-26 14:24:52 +0530

    config updates

--- next.config.js
+++ next.config.js
@@ -1,30 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true
+  }
 }
 
 module.exports = nextConfig
 
-/**
- 
-  next
-
-  1. vercel
-  2. s3 + cloudfront
-  3. amplify
-
-
-  github -> codepipeline -> codebuild -> build time -> pages -> custom script -> s3 -> cloudfront => code changes, pushed to code
-
-  sanity -> data change -> lambda -> codepipeline => CMS data changes
-
-
-
-  1. SSG -> static site generation
-  2. SSR -> server side rendering
-
-  oursite.com -> amplify -> sanity -> page generate -> SSR
-
-  oursite.com -> cloudfront -> s3 -> static files 
-
-
- */
